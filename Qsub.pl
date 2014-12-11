44d7c6a90361fb693699079f2da550c76387ea5b
ksieber <karsten.sieber@gmail.com>
2014-12-11 10:06:25 -0500

    Change to iterate over STDIN.

--- Qsub.pl
+++ Qsub.pl
@@ -13,30 +13,62 @@ if ( $options{help} ) {
     die "This script will Qsub a command. The following options can be passed.
     --cmd=          command/shell script to be qsub'ed [STDIN].
     --threads=      # cpu threads to use [1].
-    --sub_name=         Job name [STDIN].
-    --sub_mem=          min amount of RAM to use.
-    --sub_mail=         1= email {\$user}\@som.umaryland.edu or the --sub_mail\@foo.bar     
+    --sub_name=     Job name [STDIN].
+    --sub_mem=      min amount of RAM to use.
+    --sub_mail=     1= email {\$user}\@som.umaryland.edu or the --sub_mail\@foo.bar     
     --log=          file to log in.
     --project=      grid project to use [jdhotopp-lab].
     --cwd=          <0|1> [0] 1= Use current working directory. 
     --wd=           Directory for grid to work from [~/].\n";
 }
 
-my $stdin;
-if ( !$options{cmd} ) { chomp( $stdin = <> ); }
-my $cmd = defined $options{cmd} ? "$options{cmd}" : "$stdin";
-if ( $cmd !~ /\w+/ ) { die "Error: Unable to determine the cmd to qsub.\n"; }
+# my $stdin;
+# if ( !$options{cmd} ) { chomp( $stdin = <> ); }
+# my $cmd = defined $options{cmd} ? "$options{cmd}" : "$stdin";
+# if ( $cmd !~ /\w+/ ) { die "Error: Unable to determine the cmd to qsub.\n"; }
 
-my $report = Qsub(
-    {   cmd      => $cmd,
-        log      => $options{log},
-        sub_name => $options{sub_name},
-        sub_mem  => $options{sub_mem},
-        sub_mail => $options{sub_mail},
-        threads  => $options{threads},
-        project  => $options{project},
-        cwd      => $options{cwd},
-        wd       => $options{wd}
+if ( defined $options{cmd} ) {
+    my $report = Qsub(
+        {   cmd      => $options{cmd},
+            log      => $options{log},
+            sub_name => $options{sub_name},
+            sub_mem  => $options{sub_mem},
+            sub_mail => $options{sub_mail},
+            threads  => $options{threads},
+            project  => $options{project},
+            cwd      => $options{cwd},
+            wd       => $options{wd}
+        }
+    );
+}
+else {
+    while (<>) {
+        chomp( my $cmd = $_ );
+        my $report = Qsub(
+            {   cmd      => $cmd,
+                log      => $options{log},
+                sub_name => $options{sub_name},
+                sub_mem  => $options{sub_mem},
+                sub_mail => $options{sub_mail},
+                threads  => $options{threads},
+                project  => $options{project},
+                cwd      => $options{cwd},
+                wd       => $options{wd}
+            }
+        );
     }
-);
+}
+
+# my $report = Qsub(
+#     {   cmd      => $cmd,
+#         log      => $options{log},
+#         sub_name => $options{sub_name},
+#         sub_mem  => $options{sub_mem},
+#         sub_mail => $options{sub_mail},
+#         threads  => $options{threads},
+#         project  => $options{project},
+#         cwd      => $options{cwd},
+#         wd       => $options{wd}
+#     }
+# );
 
